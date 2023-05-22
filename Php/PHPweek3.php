a3b7db39b48b60244aa898b653492aaa5ea28598
Jaberrrrr <131659154+Jaberrrrr@users.noreply.github.com>
2023-05-22 10:50:30 +0200

    Add files via upload

---
+++ Php/PHPweek3.php
@@ -0,0 +1,34 @@
+<?php
+
+// $myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+
+// echo $myArray[0]. "<br>";
+// echo $myArray[1]. "<br>";
+// echo $myArray[2]. "<br>";
+// echo $myArray[3]. "<br>";
+// echo $myArray[4]. "<br>";
+// echo $myArray[5];
+
+//Deel2
+
+// $myArray = ['auto','fiets','brommer','bus','vliegtuig','trein ',' rolstoel'];
+
+// echo count($myArray);
+
+//Deel3
+
+// Bar, world, hello, 3, 5, five;
+
+
+// $cijfersPHP = [4.4, 4.6, 5.6, 6.1, 7.6, 7.2];
+
+//$gemiddelde = array_sum($cijfersPHP) / count($cijfersPHP);
+
+//echo $gemiddelde;
+
+//echo(round(5.9166666666667) . "<br>");
+
+Ik denk dat het beter is om de regels samen te voegen, omdat het makkelijker en beter uit ziet.
+
+
+?>
