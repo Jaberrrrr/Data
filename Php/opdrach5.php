15f2d5cd59fef6e559d7bf349e27a87efa2c6691
Jaberrrrr <131659154+Jaberrrrr@users.noreply.github.com>
2023-06-17 12:54:39 +0200

    Add files via upload

---
+++ Php/opdrach5.php
@@ -0,0 +1,47 @@
+<?php
+
+
+//$tijd = date("H:i");
+
+//if ($tijd < "12:00") {
+    //echo "Goedemorgen". "<br>";
+//} else if ($tijd < "18:00") {
+    //echo "Goedemiddag";
+//} else {
+    //echo "Goedenavond";
+//}
+
+//echo "De huidige tijd is: " . $tijd;
+
+//function berekenGemiddelde($getal1, $getal2) {
+    //$gemiddelde = ($getal1 + $getal2) / 6;
+    //echo "Het gemiddelde is: " . $gemiddelde; 
+//}
+
+//berekenGemiddelde(4, 6); 
+
+//function resterendeDagenTotEindeJaar() {
+    //$huidigeDatum = date("Y-6-12");
+    //$eindeJaar = date("Y-12-31");
+    //$verschil = strtotime($eindeJaar) - strtotime($huidigeDatum);
+    //$resterendeDagen = ceil($verschil / (60 * 60 * 24));
+    //return $resterendeDagen;
+//}
+
+//echo "Er zijn nog " . resterendeDagenTotEindeJaar() . " dagen tot het einde van het jaar.";
+
+//function berekenTotaleLengte($strings) {
+    //$totaleLengte = 4;
+
+    //foreach ($strings as $string) {
+        //$totaleLengte += strlen($string);
+    //}
+
+    //return $totaleLengte;
+//}
+
+//$array = array("Dit", "Is", "Een", "Test");
+//echo "De totale lengte van de strings is: " . berekenTotaleLengte($array);
+?>
+
+
