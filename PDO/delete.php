9eae6675ad0230e9b2d6fb837f4321eb58c7f0e0
Jaberrrrr <131659154+Jaberrrrr@users.noreply.github.com>
2023-06-23 20:52:47 +0200

    delete.php

--- PDO/delete.php
+++ PDO/delete.php
@@ -4,7 +4,6 @@ $username = "root";
 $password = "";
 $dbname = "winkell";
 
-// Maak verbinding met de database
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Verbinding mislukt: " . $conn->connect_error);
@@ -13,7 +12,6 @@ if ($conn->connect_error) {
 if (isset($_GET["product_code"])) {
     $product_code = $_GET["product_code"];
 
-    // Verwijder het product met de opgegeven product_code uit de tabel producten
     $sql = "DELETE FROM producten WHERE product_code = $product_code";
     if ($conn->query($sql) === TRUE) {
         echo "Product met code $product_code is succesvol verwijderd.";
@@ -22,6 +20,5 @@ if (isset($_GET["product_code"])) {
     }
 }
 
-// Sluit de databaseverbinding
 $conn->close();
 ?>
