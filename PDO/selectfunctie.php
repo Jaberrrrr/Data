aa5bfa39eddd7690f6e56c146241f6b46616bc0e
Jaberrrrr <131659154+Jaberrrrr@users.noreply.github.com>
2023-11-30 16:06:11 +0100

    Updated selectfunctie.php

--- PDO/selectfunctie.php
+++ PDO/selectfunctie.php
@@ -1,67 +1,66 @@
-    <?php
-    class Database {
-        private $host = "localhost";
-        private $user = "root";
-        private $password = "Jaber_2006";
-        private $database = "fc";
-        public $pdo;
+<?php
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "Jaber_2006";
+    private $database = "fc";
+    public $pdo;
 
-        public function __construct($database = "fc", $host = "localhost", $user = "root", $password = "Jaber_2006") {
-            try {
-                $this->pdo = new PDO("mysql:host={$host};dbname={$database}", $user, $password);
-                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "Connected";
-            } catch (PDOException $e) {
-                echo "Fout bij verbinden met de database:" . $e->getMessage();
-            }
-        }
-
-        public function selectData($id = null) {
-            try {
-                $sql = "SELECT * FROM opdracht2";
-
-                if ($id !== null) {
-                    $sql = "SELECT * FROM opdracht2 WHERE id = :id";
-                }
-
-                $stmt = $this->pdo->prepare($sql);
-
-                if ($id !== null) {
-                    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-                }
-
-                $stmt->execute();
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                return $result;
-
-            } catch (PDOException $e) {
-                echo "Fout bij selecteren van data: " . $e->getMessage();
-            }
+    public function __construct($database = "fc", $host = "localhost", $user = "root", $password = "Jaber_2006") {
+        try {
+            $this->pdo = new PDO("mysql:host={$host};dbname={$database}", $user, $password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected";
+        } catch (PDOException $e) {
+            echo "Fout bij verbinden met de database:" . $e->getMessage();
         }
     }
 
-    $db = new Database();
-    
+    public function selectData($id = null) {
+        try {
+            $sql = "SELECT * FROM opdracht2";
 
-    $data = $db->selectData();
-    printTable($data);
+            if ($id !== null) {
+                $sql = "SELECT * FROM opdracht2 WHERE id = :id";
+            }
 
-    $dataById = $db->selectData(1);
-    printTable($dataById);
+            $stmt = $this->pdo->prepare($sql);
 
-    function printTable($data) {
-        echo "<table border='1'>";
-        echo "<tr><th>ID</th><th>Naam</th><th>Leeftijd</th><th>Email</th></tr>";
-        foreach ($data as $row) {
-            echo "<tr>";
-            echo "<td>{$row['id']}</td>";
-            echo "<td>{$row['naam']}</td>";
-            echo "<td>{$row['leeftijd']}</td>";
-            echo "<td>{$row['email']}</td>";
-            echo "</tr>";
+            if ($id !== null) {
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            }
+
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $result;
+
+        } catch (PDOException $e) {
+            echo "Fout bij selecteren van data: " . $e->getMessage();
         }
-        echo "</table>";
     }
-    ?>
-                
+}
+
+$db = new Database();
+
+$data = $db->selectData();
+printTable($data);
+
+$dataById = $db->selectData(1);
+printTable($dataById);
+
+function printTable($data) {
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Naam</th><th>Leeftijd</th><th>Email</th><th>Acties</th></tr>";
+    foreach ($data as $row) {
+        echo "<tr>";
+        echo "<td>{$row['id']}</td>";
+        echo "<td>{$row['naam']}</td>";
+        echo "<td>{$row['leeftijd']}</td>";
+        echo "<td>{$row['email']}</td>";
+        echo "<td><button>Edit</button> <button>Delete</button></td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+?>
