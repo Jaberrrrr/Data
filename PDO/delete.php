c7cb901cbe35152fb649fafcfe7bfcebc98522c1
Jaberrrrr <131659154+Jaberrrrr@users.noreply.github.com>
2023-12-01 12:19:33 +0100

    Add files via upload

--- PDO/delete.php
+++ PDO/delete.php
@@ -1,24 +1,13 @@
 <?php
-$servername = "localhost:3307";
-$username = "root";
-$password = "";
-$dbname = "winkell";
+include('selectfunctie.php');
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Verbinding mislukt: " . $conn->connect_error);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $db = new Database();
+
+    $id = $_POST["id"];
+
+    $result = $db->deleteData($id);
+
+    echo $result;
 }
-
-if (isset($_GET["product_code"])) {
-    $product_code = $_GET["product_code"];
-
-    $sql = "DELETE FROM producten WHERE product_code = $product_code";
-    if ($conn->query($sql) === TRUE) {
-        echo "Product met code $product_code is succesvol verwijderd.";
-    } else {
-        echo "Fout bij het verwijderen van het product: " . $conn->error;
-    }
-}
-
-$conn->close();
 ?>
--- PDO/update.php
+++ PDO/update.php
@@ -1,61 +1,35 @@
 <?php
-
-$host = 'localhost:3307';
-$db   = 'winkell';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-
+require_once 'selectfunctie.php';
+ 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $productNaam = $_POST['product_naam'];
-    $prijsPerStuk = $_POST['prijs_per_stuk'];
-    $omschrijving = $_POST['omschrijving'];
-    $query = "INSERT INTO producten (product_naam, prijs_per_stuk, omschrijving) VALUES (:product_naam, :prijs_per_stuk, :omschrijving)";
-    try {
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(':product_naam', $productNaam);
-        $stmt->bindParam(':prijs_per_stuk', $prijsPerStuk);
-        $stmt->bindParam(':omschrijving', $omschrijving);
-        $stmt->execute();
-    } catch (PDOException $e) { $e->getMessage();
-    }
-}
+    $id = $_POST["id"];
+    $naam = $_POST["naam"];
+    $leeftijd = $_POST["leeftijd"];
+    $email = $_POST["email"];
 
-if (isset($tweedeProductNaam)) {
-    echo "<h2>Tweede product:</h2>";
-    echo "<p>Productnaam: $tweedeProductNaam</p>";
-    echo "<p>Prijs per stuk: $tweedePrijsPerStuk</p>";
-    echo "<p>Omschrijving: $tweedeOmschrijving</p>";
+    
+ 
+    $database = new Database();
+    $database->updateData($id, $naam, $leeftijd, $email);
+ 
 }
-
+ 
+ 
 ?>
+ 
+<form method="post" action="update.php">
+    <label for="id">ID:</label>
+    <input type="text" name="id" required>
+ 
+    <label for="naam">Nieuwe naam:</label>
+    <input type="text" name="naam" required>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Productformulier</title>
-</head>
-<body>
-    <form method="post" action="">
-        <label for="product_naam">Productnaam:</label>
-        <input type="text" id="product_naam" name="product_naam" required><br><br>
-        <label for="prijs_per_stuk">Prijs per stuk:</label>
-        <input type="number" id="prijs_per_stuk" name="prijs_per_stuk" required><br><br>
-        <label for="omschrijving">Omschrijving:</label><br>
-        <textarea id="omschrijving" name="omschrijving" required></textarea><br><br>
-        <input type="submit" value="Verzenden">
-    </form>
-</body>
-</html>
+    <label for="leeftijd">Nieuwe Leeftijd:</label>
+    <input type="number" name="leeftijd" required>
+    
+    <label for="email">Nieuwe email:</label>
+    <input type="text" name="email" required>
+ 
+    <input type="submit" value="Update">
+</form>
+ 
